3e6aade57d83076f1b1aa39783488cb568a76a3c
Duyen9904 <mimitrucduyen@gmail.com>
2024-06-10 00:09:28 +0700

    Add project files.

---
+++ Diamond.Data/Repository/OrderDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using Diamond.Data.Base;
+using Diamond.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond.Data.Repository
+{
+    public class OrderDetailRepository : GenericRepository<Orderdetail>
+    {
+        public OrderDetailRepository()
+        {  
+        }
+        public OrderDetailRepository(Net1814_212_3_DianContext context) => _context = context;
+        //TO-DO CODE HERE/////////////////
+    }
+}
